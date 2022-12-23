fcfc8b53d188ff08691484330956342605a38a4b
Shola Azeez <sholaaz1805@gmail.com>
2022-12-23 22:54:46 +0000

    sideBar

--- src/App.js
+++ src/App.js
@@ -7,36 +7,63 @@ import GoToTop from "./GoToTop";
 import "./App.css";
 import Waitlist from "./pages/waitlist/WaitlistForm";
 import Event from "./event/Event"
-
+import Settings from "./event/Settings";
+import EventHome from "./event/EventHome";
+import Report from "./event/Report"
+ 
 function App() {
   return (
-    <BrowserRouter>
+      // <>
       <>
-        <GoToTop />
+      <>
+        <GoToTop /> 
       </>
+      <>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        {/* <Route path="/sponsors" element={<Sponsors />} /> */}
-        {/* <Route path="/organizers" element={<Organizers />} /> */}
-        <Route path="/contactUs" element={<ContactUs />} />
-        {/* <Route path="/aboutUs" element={<AboutUs />} /> */}
-        {/* <Route path="/contactUs" element={<ContactUs />} /> */}
-        <Route path="/aboutUs" element={<AboutUs />} />
-        {/* <Route path="/logIn" element={<SignIn />} /> */}
-        {/* <Route path="/forgotPassword" element={<ForgotPassword />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
-          <Route path="/resetPasswordNew" element={<ResetSignIn/>} />
-          <Route path="/resetSuccessful" element={<ResetSuccessful/>} />
-          <Route path="/eventreportupload" element={<EventReport/>} />
-          <Route path="/eventcategories" element={<EventCategories/>} />
-          <Route path="/continue" element={<ContinueModal/>} />
-          <Route path='/eventmanagement' element={<Event/>} />
-          <Route path="/verifyOtp" element={<VerifyOTP/>} /> */}
-        <Route path="/waitlist" element={<Waitlist />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="*" element={<NoPage />} />
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/contactUs" element={<ContactUs />} />
+      <Route path="/aboutUs" element={<AboutUs />} />
       </Routes>
-    </BrowserRouter>
+      </>
+      <>
+      <Event/>
+      </>
+      <>
+      <Routes>
+        <Route path="/eventHome" element={<EventHome/>} />
+        <Route path="/report" element={<Report/>} />
+        <Route path="/settings" element={<Settings/>} />
+      </Routes>
+      </>
+      </>
+      
+      // <Routes>
+      //   <Route path="/" element={<Home />}></Route>
+      //   {/* <Route path="/sponsors" element={<Sponsors />} /> */}
+      //   {/* <Route path="/organizers" element={<Organizers />} /> */}
+      //   <Route path="/contactUs" element={<ContactUs />} />
+      //   {/* <Route path="/aboutUs" element={<AboutUs />} /> */}
+      //   {/* <Route path="/contactUs" element={<ContactUs />} /> */}
+      //   <Route path="/aboutUs" element={<AboutUs />} />
+      //   {/* <Route path="/logIn" element={<SignIn />} /> */}
+      //   {/* <Route path="/forgotPassword" element={<ForgotPassword />} />
+      //     <Route path="/resetPassword" element={<ResetPassword />} />
+      //     <Route path="/resetPasswordNew" element={<ResetSignIn/>} />
+      //     <Route path="/resetSuccessful" element={<ResetSuccessful/>} />
+      //     <Route path="/eventreportupload" element={<EventReport/>} />
+      //     <Route path="/eventcategories" element={<EventCategories/>} />
+      //     <Route path="/continue" element={<ContinueModal/>} />
+      //     <Route path='/eventmanagement' element={<Event/>} />
+      //     <Route path="/verifyOtp" element={<VerifyOTP/>} /> */}
+          
+      //   <Route path="/waitlist" element={<Waitlist />} />
+      //   <Route path="/event" element={<Event />}>
+        
+      //   </Route>
+      //   <Route path="*" element={<NoPage />} />
+      // </Routes>
+   
+      // </>
   );
 }
 
