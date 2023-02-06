87f4ed469ce0dde69cd9bf06fc50f1e2ad7d93eb
Josiah ENUMAH <josiahenumah94@gmail.com>
2023-02-06 08:53:40 +0100

    updates for integration

--- src/event/Dashboard/SidebarData.js
+++ src/event/Dashboard/SidebarData.js
@@ -20,7 +20,7 @@ export const SidebarData = [
     // icon: <AiIcons.AiFillHome />,
     icon: (
       <Square2StackIcon
-        class="h-6 h-6 text-gray-500"
+        className="h-6 h-6 text-gray-500"
         style={{ width: "20px" }}
       />
     ),
@@ -31,7 +31,10 @@ export const SidebarData = [
     // path: "/event",
     // icon: <HiIcons.HiOutlineCalendar />
     icon: (
-      <CalendarIcon class="h-6 h-6 text-gray-500" style={{ width: "20px" }} />
+      <CalendarIcon
+        className="h-6 h-6 text-gray-500"
+        style={{ width: "20px" }}
+      />
     ),
 
     iconClosed: <MdIcons.MdKeyboardArrowRight />,
@@ -59,7 +62,7 @@ export const SidebarData = [
     // icon: <HiIcons.HiOutlineDocumentText />,
     icon: (
       <DocumentChartBarIcon
-        class="h-6 h-6 text-gray-500"
+        className="h-6 h-6 text-gray-500"
         style={{ width: "20px" }}
       />
     ),
@@ -85,7 +88,7 @@ export const SidebarData = [
     path: "/messaging",
     icon: (
       <ChatBubbleLeftRightIcon
-        class="h-6 h-6 text-gray-500"
+        className="h-6 h-6 text-gray-500"
         style={{ width: "20px" }}
       />
     ),
@@ -93,13 +96,18 @@ export const SidebarData = [
   {
     title: "Notifications",
     path: "/notification",
-    icon: <BellIcon class="h-6 h-6 text-gray-500" style={{ width: "20px" }} />,
+    icon: (
+      <BellIcon className="h-6 h-6 text-gray-500" style={{ width: "20px" }} />
+    ),
   },
   {
     title: "Attendees Mgt",
     path: "/attendeesmgt",
     icon: (
-      <UserGroupIcon class="h-6 h-6 text-gray-500" style={{ width: "20px" }} />
+      <UserGroupIcon
+        className="h-6 h-6 text-gray-500"
+        style={{ width: "20px" }}
+      />
     ),
   },
   {
@@ -107,7 +115,7 @@ export const SidebarData = [
     path: "/vendorsmgt",
     icon: (
       <Square2StackIcon
-        class="h-6 h-6 text-gray-500"
+        className="h-6 h-6 text-gray-500"
         style={{ width: "20px" }}
       />
     ),
@@ -116,13 +124,18 @@ export const SidebarData = [
     title: "Analytics",
     path: "/analytics",
     icon: (
-      <ChartBarIcon class="h-6 h-6 text-gray-500" style={{ width: "20px" }} />
+      <ChartBarIcon
+        className="h-6 h-6 text-gray-500"
+        style={{ width: "20px" }}
+      />
     ),
   },
 
   {
     title: "Settings",
     path: "/settings",
-    icon: <CogIcon class="h-6 h-6 text-gray-500" style={{ width: "20px" }} />,
+    icon: (
+      <CogIcon className="h-6 h-6 text-gray-500" style={{ width: "20px" }} />
+    ),
   },
 ];
--- src/profile/socialProfile/SocialProfile.js
+++ src/profile/socialProfile/SocialProfile.js
@@ -1,108 +1,260 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { KCLogo, LogoDiv, ProfileContainer, ProfileContent, ProfileProgress, ProfileSection, ShapedBackground } from '../createProfile/CreateProfileStyled';
+import React, { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import {
+  KCLogo,
+  LogoDiv,
+  ProfileContainer,
+  ProfileContent,
+  ProfileProgress,
+  ProfileSection,
+  ShapedBackground,
+} from "../createProfile/CreateProfileStyled";
 import kingCabanaLogo from "../../images/kingCabanaLogo.svg";
-import { EventHeader1 } from '../../event/createEvent/TimeLineEventsStyled';
-import { ButtonSave, InputSeg, SaveBox, TransparentButton } from '../organiserProfile/OrganiserProfileStyled';
-import { CheckBoxInput, Input, InputBoxOther, InputText, SmallText } from '../../event/createEvent/FirstCreateEventStyled';
-import { DownButtonFull } from '../../event/createEvent/SecondCreateEventStyled';
-
-
+import { EventHeader1 } from "../../event/createEvent/TimeLineEventsStyled";
+import {
+  ButtonSave,
+  InputSeg,
+  SaveBox,
+  TransparentButton,
+} from "../organiserProfile/OrganiserProfileStyled";
+import {
+  CheckBoxInput,
+  Input,
+  InputBoxOther,
+  InputText,
+  SmallText,
+} from "../../event/createEvent/FirstCreateEventStyled";
+import { DownButtonFull } from "../../event/createEvent/SecondCreateEventStyled";
+import { editProfile } from "../../redux/slices/profileSlice";
 
 const SocialProfile = () => {
   const [visibility, setVisibility] = useState(false);
   const navigate = useNavigate();
+  const inputRef = useRef();
 
-  const navigateNext = () =>{
-    navigate('/manageProfile');
+  const toggleOthers = () => {
+    if (visibility === true) {
+      setVisibility(false);
+    }
+  };
+
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.profile);
+
+  const change = (e) => {
+    dispatch(editProfile({ name: e.target.name, value: e.target.value }));
+  };
+
+  const navigateNext = () => {
+    navigate("/manageProfile");
   };
   const navigateBack = () => {
-    navigate('/organiserProfile')
-  }
+    navigate("/organiserProfile");
+  };
 
   return (
     <>
       <ProfileContainer>
         <ShapedBackground />
-          <ProfileContent>
-            <LogoDiv>
-              <KCLogo src={kingCabanaLogo} alt='kcLogo'/>
-            </LogoDiv>
-              <ProfileSection>
-                <ProfileProgress>Step 3 of 4</ProfileProgress>    
-                <EventHeader1>Social Media & Guarantor information</EventHeader1>
-                
-                <InputText>Social media information</InputText>
-                <InputSeg>
-                  <InputText>Website</InputText>
-                  <Input type='text' placeholder='Enter URL' />
-                </InputSeg>
+        <ProfileContent>
+          <LogoDiv>
+            <KCLogo src={kingCabanaLogo} alt="kcLogo" />
+          </LogoDiv>
+          <ProfileSection>
+            <ProfileProgress>Step 3 of 4</ProfileProgress>
+            <EventHeader1>Social Media & Guarantor information</EventHeader1>
 
-                <InputSeg>
-                  <InputText>Linkedin</InputText>
-                  <Input type='text' placeholder='Enter URL' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Instagram</InputText>
-                  <Input type='text' placeholder='Enter URL' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Twitter</InputText>
-                  <Input type='text' placeholder='Enter URL' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Facebook</InputText>
-                  <Input type='text' placeholder='Enter URL' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Others</InputText>
-                  <Input type='text' placeholder='Enter URL' />
-                </InputSeg>
+            <InputText>Social media information</InputText>
+            <InputSeg>
+              <InputText>Website</InputText>
+              <Input
+                type="text"
+                placeholder="Enter URL"
+                name="website"
+                value={state.website}
+                onChange={change}
+              />
+            </InputSeg>
 
-                <InputSeg>
-                    <InputText>Guarantor's information</InputText>
-                    <SmallText>Select a role that befits a guarantor to this event community</SmallText>
-                    <CheckBoxInput><input type="radio" id='patron' name='patron'/><label htmlFor='patron'>Patron</label></CheckBoxInput>
-                    <CheckBoxInput><input type="radio" id='staff' name='staff'/><label htmlFor='staff'>Staff Adviser</label></CheckBoxInput>
-                    <CheckBoxInput><input type="radio" id='coordinators' name='coordinators'/><label htmlFor='coordinators'>Coordinators</label></CheckBoxInput>
-                    <CheckBoxInput><input onClick={() => setVisibility(!visibility)} type="radio" id='patron' name='patron'/><label htmlFor='patron'>Others (please specify)</label></CheckBoxInput>
-                    <InputBoxOther display={visibility ? "flex" : "none"} ><Input type='text' placeholder='Enter others' /></InputBoxOther>
-                </InputSeg>
+            <InputSeg>
+              <InputText>Linkedin</InputText>
+              <Input
+                type="text"
+                placeholder="Enter URL"
+                name="linkedin"
+                value={state.linkedin}
+                onChange={change}
+              />
+            </InputSeg>
 
-                <InputText>Full name of (Secondary Contact)</InputText>
-                <InputSeg>
-                  <InputText>Company/Business Name</InputText>
-                  <Input type='text' placeholder='Enter Company/Business Name' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Job Role</InputText>
-                  <Input type='text' placeholder='Enter Job Role' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Office Address</InputText>
-                  <Input type='text' placeholder='Enter Office Address' />
-                </InputSeg>
-                
-                <InputSeg>
-                  <InputText>Phone Number</InputText>
-                  <Input type='number' placeholder='Enter Phone Number' />
-                </InputSeg>
+            <InputSeg>
+              <InputText>Instagram</InputText>
+              <Input
+                type="text"
+                placeholder="Enter URL"
+                name="instagram"
+                value={state.instagram}
+                onChange={change}
+              />
+            </InputSeg>
 
-                <InputSeg>
-                  <InputText>Email Address</InputText>
-                  <Input type='email' placeholder='Enter Email' />
-                </InputSeg>
+            <InputSeg>
+              <InputText>Twitter</InputText>
+              <Input
+                type="text"
+                placeholder="Enter URL"
+                name="twitter"
+                value={state.twitter}
+                onChange={change}
+              />
+            </InputSeg>
 
-              </ProfileSection>
-          </ProfileContent>
-        
-          <SaveBox>
+            <InputSeg>
+              <InputText>Facebook</InputText>
+              <Input
+                type="text"
+                placeholder="Enter URL"
+                name="facebook"
+                value={state.facebook}
+                onChange={change}
+              />
+            </InputSeg>
+
+            <InputSeg>
+              <InputText>Others</InputText>
+              <Input
+                type="text"
+                placeholder="Enter URL"
+                name="others"
+                value={state.others}
+                onChange={change}
+              />
+            </InputSeg>
+
+            <InputSeg>
+              <InputText>Guarantor's information</InputText>
+              <SmallText>
+                Select a role that befits a guarantor to this event community
+              </SmallText>
+              <CheckBoxInput>
+                <input
+                  type="radio"
+                  id="patron"
+                  ref={inputRef}
+                  name="guarantorInformation"
+                  value={state.guarantorInformation}
+                  onChange={change}
+                  onClick={toggleOthers}
+                />
+                <label htmlFor="patron">Patron</label>
+              </CheckBoxInput>
+              <CheckBoxInput>
+                <input
+                  type="radio"
+                  id="staff"
+                  ref={inputRef}
+                  name="guarantorInformation"
+                  value={state.guarantorInformation}
+                  onChange={change}
+                  onClick={toggleOthers}
+                />
+                <label htmlFor="staff">Staff Adviser</label>
+              </CheckBoxInput>
+              <CheckBoxInput>
+                <input
+                  type="radio"
+                  id="coordinators"
+                  ref={inputRef}
+                  name="guarantorInformation"
+                  value={state.guarantorInformation}
+                  onChange={change}
+                  onClick={toggleOthers}
+                />
+                <label htmlFor="coordinators">Coordinators</label>
+              </CheckBoxInput>
+              <CheckBoxInput>
+                <input
+                  onClick={() => setVisibility(!visibility)}
+                  type="radio"
+                  id="patron"
+                  ref={inputRef}
+                  name="guarantorInformation"
+                  onChange={change}
+                  value={state.guarantorInformation}
+                />
+                <label htmlFor="patron">Others (please specify)</label>
+              </CheckBoxInput>
+              <InputBoxOther display={visibility ? "flex" : "none"}>
+                <Input
+                  type="text"
+                  placeholder="Enter others"
+                  name="guarantorsInformation"
+                  value={state.guarantorInformation}
+                  onChange={change}
+                />
+              </InputBoxOther>
+            </InputSeg>
+
+            <InputText>Full name of {"{Secondary Contact}"}</InputText>
+            <InputSeg>
+              <InputText>Company/Business Name</InputText>
+              <Input
+                type="text"
+                placeholder="Enter Company/Business Name"
+                name="company"
+                value={state.company}
+                onChange={change}
+              />
+            </InputSeg>
+
+            <InputSeg>
+              <InputText>Job Role</InputText>
+              <Input
+                type="text"
+                placeholder="Enter Job Role"
+                name="jobRole"
+                value={state.jobRole}
+                onChange={change}
+              />
+            </InputSeg>
+
+            <InputSeg>
+              <InputText>Office Address</InputText>
+              <Input
+                type="text"
+                placeholder="Enter Office Address"
+                name="officeAddress"
+                value={state.officeAddress}
+                onChange={change}
+              />
+            </InputSeg>
+
+            <InputSeg>
+              <InputText>Phone Number</InputText>
+              <Input
+                type="number"
+                placeholder="Enter Phone Number"
+                name="guarantorNumber"
+                value={state.guarantorNumber}
+                onChange={change}
+              />
+            </InputSeg>
+
+            <InputSeg>
+              <InputText>Email Address</InputText>
+              <Input
+                type="email"
+                placeholder="Enter Email"
+                name="guarantorEmail"
+                value={state.guarantorEmail}
+                onChange={change}
+              />
+            </InputSeg>
+          </ProfileSection>
+        </ProfileContent>
+
+        <SaveBox>
           <ButtonSave>
             <TransparentButton onClick={navigateBack}>Back</TransparentButton>
             <DownButtonFull onClick={navigateNext}>Next</DownButtonFull>
@@ -110,7 +262,7 @@ const SocialProfile = () => {
         </SaveBox>
       </ProfileContainer>
     </>
-  )
-}
+  );
+};
 
-export default SocialProfile
+export default SocialProfile;
