1602d34a70f1147ade2988ecfecde06ba0d29a83
Alabi Tobi Oladipupo <100195660+Ordinaka@users.noreply.github.com>
2023-02-06 09:23:07 +0100

    configure toolkit shola

--- src/redux/slices/profileSlice.js
+++ src/redux/slices/profileSlice.js
@@ -12,7 +12,7 @@ const initialState = {
   others: "",
   guarantorInformation: "",
   company: "",
-  jobrole: "",
+  jobRole: "",
   officeAddress: "",
   guarantorNumber: "",
   guarantorEmail: "",
